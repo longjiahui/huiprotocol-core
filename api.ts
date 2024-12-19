21ea84bbd23efc8027fbb82efc1105c08ace727b
Anfo <longjiahui@hotmail.com>
2024-12-19 15:02:35 +0800

    feat: add requestConfigType

--- api.ts
+++ api.ts
@@ -1,3 +1,4 @@
+import { Slice1Parameters } from "@anfo/huiserver"
 import {
   Method,
   APIInstance,
@@ -10,11 +11,17 @@ import {
   IsPaginationAPI,
 } from "./url"
 
-export function createAPIType(
-  request: (options: { url: string; method: Method; data?: any }) => any
+export function createAPIType<RequestConfig>(
+  request: (
+    options: { url: string; method: Method; data?: any },
+    config?: RequestConfig,
+  ) => any,
 ) {
   // 柯里化、让泛型推导一部分类型（URL）
-  class API<T extends APIInstance> {
+  class API<
+    T extends APIInstance,
+    RequestConfigType extends RequestConfig = RequestConfig,
+  > {
     url: T["path"]
     method: Method
 
@@ -22,23 +29,39 @@ export function createAPIType(
       this.url = api.path
       this.method = api.method
     }
-    call(
+    config(config: RequestConfigType) {
+      return Object.assign(this, {
+        call: (...rest: Parameters<typeof this.call>) =>
+          this._callWithConfig(config, ...rest),
+      })
+    }
+
+    _callWithConfig(
+      config: RequestConfigType | undefined,
       ...rest: GetAPIReq<T> extends void
         ? GetAPIPathParameters<T>
         : [
             ...GetAPIPathParameters<T>,
-            ...(GetAPIReq<T> extends {} ? [GetAPIReq<T>?] : [GetAPIReq<T>])
+            ...(GetAPIReq<T> extends {} ? [GetAPIReq<T>?] : [GetAPIReq<T>]),
           ]
     ) {
       const params = rest.slice(this.url.length)[0]
       const urlParams = rest
         .slice(0, this.url.length)
         .map((d) => d || "") as string[]
-      return request({
-        url: typeof this.url === "function" ? this.url(...urlParams) : this.url,
-        method: this.method,
-        data: params,
-      }) as Promise<GetAPIRes<T>>
+      return request(
+        {
+          url:
+            typeof this.url === "function" ? this.url(...urlParams) : this.url,
+          method: this.method,
+          data: params,
+        },
+        config,
+      ) as Promise<GetAPIRes<T>>
+    }
+
+    call(...rest: Slice1Parameters<typeof this._callWithConfig>) {
+      return this._callWithConfig(undefined, ...rest)
     }
   }
 
@@ -54,7 +77,7 @@ export function createAPIType(
             ...GetAPIPathParameters<T>,
             ...(GetPaginationExtraParams<T> extends {}
               ? [GetPaginationExtraParams<T>?]
-              : [GetPaginationExtraParams<T>])
+              : [GetPaginationExtraParams<T>]),
           ]
     ) {
       return this.call(
@@ -67,7 +90,7 @@ export function createAPIType(
               rest.slice(this.url.length) as GetPaginationExtraParams<T>[]
             )?.[0] || {}),
           },
-        ] as unknown as Parameters<typeof this.call>)
+        ] as unknown as Parameters<typeof this.call>),
       )
     }
   }
@@ -76,12 +99,17 @@ export function createAPIType(
     PaginationAPI,
   }
 }
-
+type APIType<RequestConfig = any> = ReturnType<
+  typeof createAPIType<RequestConfig>
+>
+type GetAPITypeRequestConfigType<T extends APIType> =
+  T extends APIType<infer R> ? R : never
 // type Key = keyof typeof urls
 export function createAPIs<
+  API extends APIType,
   URLs extends Record<any, APIInstance | PaginationAPIInstance>,
-  Key extends keyof URLs
->(apiType: ReturnType<typeof createAPIType>, urls: URLs) {
+  Key extends keyof URLs,
+>(apiType: API, urls: URLs) {
   return Object.keys(urls).reduce(
     (t, k) => {
       t[k as Key] = isPaginationAPI(urls[k as Key])
@@ -92,7 +120,12 @@ export function createAPIs<
     {} as {
       [k in Key]: IsPaginationAPI<(typeof urls)[k]> extends true
         ? InstanceType<typeof apiType.PaginationAPI<(typeof urls)[k]>>
-        : InstanceType<typeof apiType.API<(typeof urls)[k]>>
-    }
+        : InstanceType<
+            typeof apiType.API<
+              (typeof urls)[k],
+              GetAPITypeRequestConfigType<API>
+            >
+          >
+    },
   )
 }
